ddd9232e199772d3af2a5537fbe22133e2b59cb7
sezergemtsov <sezer.gemtsov@ya.ru>
2022-10-05 15:25:00 +0300

    Lock logic updated

--- Problem2_Auto2/src/main/java/ImprovedCarShop.java
+++ Problem2_Auto2/src/main/java/ImprovedCarShop.java
@@ -1,45 +1,53 @@
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ImprovedCarShop extends CarShop {
 
     ReentrantLock lock;
+    Condition isEmpty;
 
     public ImprovedCarShop(CarFactory carFactory) {
         super(carFactory);
         lock = new ReentrantLock(true);
+        isEmpty = lock.newCondition();
     }
 
     @Override
     public Car addCar() {
-        synchronized (cars) {
+        lock.lock();
+        try {
             Car car = carFactory.call();
-            try {
-                lock.lockInterruptibly();
-                cars.add(car);
-                cars.notify();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
+            cars.add(car);
+            isEmpty.signalAll();
+            return car;
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
                 lock.unlock();
             }
-            return car;
         }
     }
 
     @Override
     public Car popCar() {
-        synchronized (cars) {
-            if (cars.isEmpty()) {
-                try {
-                    cars.wait();
-                    lock.lock();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    lock.unlock();
-                }
+        int pr = lock.getHoldCount();
+        if (pr >= 10) {
+            pr = 10;
+        } else {
+            pr = 10 - pr;
+        }
+        Thread.currentThread().setPriority(pr);
+        lock.lock();
+        try {
+            while (cars.isEmpty()) {
+                isEmpty.await();
             }
             return cars.pop();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
         }
     }
 
